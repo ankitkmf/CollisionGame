f0cb1e25d5bbd521a1375d397509bbdba1fab2c3
Ankit Goel <ankit.goel@dnata.com>
2017-09-24 18:27:12 +0400

    AI1

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $("document").ready(function() {
     var tickInterval;
     $("#start").click(function() {
         var childInitialOffset = $(".block").offset().left;
-        tickInterval = setInterval("runCar(" + childInitialOffset + ");", 800);
+        tickInterval = setInterval("runCar(" + childInitialOffset + ");", 400);
     });
 
     $("#stop").click(function() {
@@ -16,18 +16,18 @@ let stopCar = (tickInterval) => {
 };
 
 let runCar = (childInitialOffset) => {
-    var block = $(".block");
+    var car = $(".car");
     var area = $(".area");
 
     var parentInitialOffset = $(".area").offset().left;
-    var blockOffset = block.offset().left + block.outerWidth();
+    var carOffset = car.offset().left + car.outerWidth();
     var areaOffset = area.offset().left + area.outerWidth();
     var restartflag = false;
 
-    if (blockOffset < areaOffset) {
-        block.animate({ 'marginLeft': "+=30px" });
+    if (carOffset < areaOffset) {
+        car.animate({ 'marginLeft': "+=50px" });
     } else {
-        $('.block').css({ marginLeft: 0 }); //.offset(childInitialOffset);
+        $('.car').css({ marginLeft: 0 }); //.offset(childInitialOffset);
     }
-    console.log("child:" + blockOffset + " , parent:" + areaOffset + " ,childInitialOffset:" + childInitialOffset);
+    //  console.log("child:" + carOffset + " , parent:" + areaOffset + " ,childInitialOffset:" + childInitialOffset);
 };
